3de3f47c8998d5a737ed51f0bc4a93a9e0a3e6c6
Lenz Gschwendtner <lenz@springtimesoft.com>
2015-09-23 20:02:15 +1200

    initial commit

---
+++ lib/basics.ex
@@ -0,0 +1,2 @@
+defmodule Basics do
+end
---
+++ test/basics_test.exs
@@ -0,0 +1,11 @@
+defmodule BasicsTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  test "second test" do
+    assert true == true
+  end
+end
